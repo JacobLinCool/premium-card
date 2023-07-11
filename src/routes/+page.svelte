7e86743645f09bb8a4c6dfa1209f4b3533fb9eed
JacobLinCool <jacoblincool@gmail.com>
2023-07-12 02:41:14 +0800

    feat: add favicon and title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,10 @@
 	let expiry = (browser && $page.url.searchParams.get("expiry")) || "2023.07 ~ 2024.06";
 </script>
 
+<svelte:head>
+	<title>Premium Card</title>
+</svelte:head>
+
 <div class="flex h-full w-full items-center justify-center bg-zinc-950">
 	<a href="https://github.com/JacobLinCool/premium-card" target="_blank" class="contents">
 		<Card {membership} {org} {owner} {id} {expiry} />
