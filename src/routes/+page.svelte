c5c985963a30a6ca0a046750f8007cfb5f158d37
JacobLinCool <jacoblincool@gmail.com>
2023-07-12 21:21:41 +0800

    fix: allow empty string

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,12 @@
 	import { page } from "$app/stores";
 	import { browser } from "$app/environment";
 
-	let membership = (browser && $page.url.searchParams.get("membership")) || "CSIE Premium Club";
-	let org = (browser && $page.url.searchParams.get("org")) || "NTNU CSIE SA 39th";
-	let owner = (browser && $page.url.searchParams.get("owner")) || "Jacob Lin";
-	let id = (browser && $page.url.searchParams.get("id")) || "41047029S";
-	let expiry = (browser && $page.url.searchParams.get("expiry")) || "2023.07 ~ 2024.06";
+	let membership =
+		((browser && $page.url.searchParams.get("membership")) ?? "CSIE Premium Club") || "";
+	let org = ((browser && $page.url.searchParams.get("org")) ?? "NTNU CSIE SA 39th") || "";
+	let owner = ((browser && $page.url.searchParams.get("owner")) ?? "Jacob Lin") || "";
+	let id = ((browser && $page.url.searchParams.get("id")) ?? "41047029S") || "";
+	let expiry = ((browser && $page.url.searchParams.get("expiry")) ?? "2023.07 ~ 2024.06") || "";
 </script>
 
 <svelte:head>
